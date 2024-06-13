45975d0648a808c8ba4a3625f8326c042e01c272
juanidaste <9343@holbertonstudents.com>
2024-06-13 17:00:41 +0000

    commit 4

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "ejercicio 5.
- *       
+ * main - Prints 
+ *                
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        printf("\"with proper grammar,but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 
         return (0);
 }
