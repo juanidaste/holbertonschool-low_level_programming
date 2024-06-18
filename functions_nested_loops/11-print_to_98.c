7af2e466d31637ff958cd2620a689dc0e455fe56
juanidaste <9343@holbertonstudents.com>
2024-06-18 20:40:42 +0000

    commit 6

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,49 @@
 #include "main.h"
-
+#include "11.01-putchar.c"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_to_98 - Prints from number N to 98
+ * @n: Natural number
+ * Return: No return
  */
-int main(void)
+void print_to_98(int n)
 {
-    print_to_98(0);
-    print_to_98(98);
-    print_to_98(111);
-    print_to_98(81);
-    print_to_98(-10);
-    return (0);
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n != 98)
+			{
+				if (n > 9)
+					_printresult10(n);
+				else if (n >= 0 && n <= 9)
+				{
+					_putchar(n + '0');
+					_putchar(',');
+					_putchar(' ');
+				} else if (n < 0 && n >= -9)
+					_printresultnegativesimple(n);
+				else if (n >= -99)
+					_printresultnegative10(n);
+				else
+					_printresultnegative100(n);
+			} else
+				_printresultsimple(n);
+			n++;
+		}
+	} else
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+			{
+				if (n > 99)
+					_printresultpositive100(n);
+				else
+					_printresult10(n);
+			} else
+				_printresultsimple(n);
+			n--;
+		}
+	}
+
 }
