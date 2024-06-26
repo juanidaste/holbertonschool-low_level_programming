222ccfeffd36948fbb8ef70c795980e941c318e7
juanidaste <9343holbertonstudents.com>
2024-06-26 02:35:36 +0000

    commit 7

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,19 +1,37 @@
-int _atoi(char *s) {
-    int signo = 1;
-    int resultado = 0;
+#include "main.h"
 
-    while (*s!= '\0') {
-        if (*s == '-') {
-            signo = -signo;
-        } else if (*s == '+') {
-            signo = signo; // no hacemos nada, solo ignoramos el +
-        } else if (*s >= '0' && *s <= '9') {
-            resultado = resultado * 10 + (*s - '0');
-        } else if (resultado!= 0) {
-            break; // hemos encontrado un número, paramos de parsear
-        }
-        s++;
-    }
+/**
+ * _atoi - convierte una string a integer
+ *
+ * @s: s
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int x = 0;
+	int i = 0;
+	int simb = -1;
+	int res = 0;
 
-    return signo * resultado;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			simb *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res *= 10;
+			res -= (s[i] - '0');
+			x = 1;
+		}
+
+		else if (x == 1)
+			break;
+		i++;
+	}
+	res *= simb;
+
+	return (res);
+
 }
