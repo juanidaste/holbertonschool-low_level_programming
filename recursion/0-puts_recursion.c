06d7faa3afc1d66ba0dd5b3b311f8eac07e8442b
juanidaste <9343@holbertonstudents.com>
2024-06-26 14:45:41 +0000

    commit 8

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * _puts_recursion - print a str
+ * @s: string
+ */
+ 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0');
+	if (*s == '\0')
 	{
-		_putchar('\n')
+		_putchar('\n');
 		return;
 	}
 
