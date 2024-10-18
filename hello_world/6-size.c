ef797828f033a7017781371237be85c792f07afe
juanidaste <juanidaste@gmail.com>
2024-10-18 15:15:22 +0000

    commit mamasita

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - lol
+ *
+ * Return: 0 abdiadoiadua
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Sizeof a long: %lu byte(s)\n", sizeof(long int));
+	printf("Sizeof a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Sizeof a float: %lu byte(s)\n", sizeof(float));
+	return(0);
+}	
