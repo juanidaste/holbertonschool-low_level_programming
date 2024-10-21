53eb60a8f69293cda45d0d7b322cf3657b0b4001
juanidaste <juanidaste@gmail.com>
2024-10-21 17:46:52 +0000

    commit 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,16 @@
 int main(void)
 {
 char alph = 'a';
-
+char ALPH = 'A';
 	while (alph <= 'z')
 {
 	putchar(alph);
 	alph++;
+}
+	while (ALPH <= 'Z')
+{
+	putchar(ALPH);
+	ALPH++;
 }
 	putchar('\n');
 	return (0);
