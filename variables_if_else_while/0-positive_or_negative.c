c6912b53d2c34c6ce6f08590589bf75278c70d25
juanidaste <juanidaste@gmail.com>
2024-10-21 16:18:14 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 
 /**
- * This program will assign a random number to the variable n each time it is executed.
+ * main - This program will assign a random number
+ *
+ *to the variable n each time it is executed.
+ * Return: 0
  */
 
 int main(void)
