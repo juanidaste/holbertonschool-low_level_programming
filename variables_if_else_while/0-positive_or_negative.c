4a1654495d1cfab59722626c95075725fdfa0dc8
juanidaste <juanidaste@gmail.com>
2024-10-21 16:15:57 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * This program will assign a random number to the variable n each time it is executed.
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	else printf("%d is zero\n", n);
+	else
+	printf("%d is zero\n", n);
 	return (0);
 }
