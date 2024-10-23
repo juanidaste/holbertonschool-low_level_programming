71ce7aaef2664f1231c010c30cc9387add071be6
juanidaste <juanidaste@gmail.com>
2024-10-23 14:02:54 +0000

    commit 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - This program will assign a random number
+ *
+ *to the variable n each time it is executed.
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int luquitas = '0';
+
+	while (luquitas <= 9)
+
+	{
+		printf("%d", luquitas);
+			luquitas++;
+}
+	printf("\n");
+	return (0);
+}
