32e7542e30db0c66164353a43a35bd3a0ee6e1f2
juanidaste <juanidaste@gmail.com>
2024-10-23 14:23:03 +0000

    commit 5

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
+/**
+ * main - This program will assign a random number
+ *
+ *to the variable n each time it is executed.
+ * Return: 0
+ */
 
 int main(void)
 {
 	int unoalnueve;
-	
+
 	unoalnueve = 0;
 
-	while(unoalnueve <= 9)
+	while (unoalnueve <= 9)
 {
 	putchar(unoalnueve + '0');
 }
 	putchar('\n');
-	return(0);
+	return (0);
 }
