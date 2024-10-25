d344b01d708a548000ee04d4c2ca5c6cdc47cd84
juanidaste <juanidaste@gmail.com>
2024-10-25 15:39:43 +0000

    commit 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
+/**
+ * main - This program will assign a random number
+ *
+ *to the variable n each time it is executed.
+ * Return: 0
+ */
 
 int main(void)
 {
 
 	int num;
 
-	for(num = 0; num < 10; num++)
-{       
+	for (num = 0; num < 10; num++)
+{
 	putchar(num + '0');
-	if(num != 9)
+	if (num != 9)
 {
 	putchar(',');
 	putchar(' ');
 }
 }
 	putchar('\n');
-	return(0);
-
+	return (0);
 }
