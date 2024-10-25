433ad14dd7cf637819a8307960b4c9ece4c738dc
juanidaste <juanidaste@gmail.com>
2024-10-25 13:44:27 +0000

    commit 6

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - This program will assign a random number
+ *
+ *to the variable n each time it is executed.
+ * Return: 0
+ */
+
+int main (void)
+
+{
+        char alph;
+	int num;
+
+        alph = 'a';
+	num = '0';
+
+	while (num <= 10)
+	{
+
+		putchar(num);
+		num++;
+	}
+
+
+        while (alph <= 'f')
+        {
+                putchar(alph);
+                alph++;
+        }
+        putchar('\n');
+        return (0);
+}
