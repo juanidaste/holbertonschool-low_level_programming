3adc230cbdaffe6ce9e5270c243386bf58c25d09
juanidaste <juanidaste@gmail.com>
2024-10-25 17:33:01 +0000

    commit 9

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * main - This program will assign a random number
+ *print_alphabet - This program will assign a random number
  *
  *to the variable n each time it is executed.
- * Return: 0
  */
 
 void print_alphabet(void)
 {
 char alph = 'a';
 
-        while (alph <= 'z')
+	while (alph <= 'z')
 {
-        _putchar(alph);
-        alph++;
+	_putchar(alph);
+	alph++;
 }
-        _putchar('\n');
+	_putchar('\n');
 
 }
