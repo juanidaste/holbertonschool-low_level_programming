301b2450abfe051efca2b7902a62e1007f10257f
juanidaste <juanidaste@gmail.com>
2024-10-28 17:41:12 +0000

    commit 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - The function check uppercase letter
+ * @c: parameter to be checked 
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
