2800f8c37dabe15ad52d13f366059fe5768ffde1
juanidaste <juanidaste@gmail.com>
2024-10-28 02:33:55 +0000

    commit 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-#include "main.h"
+#include<stdio.h>
+#include"main.h"
+
 /**
  * _islower - Entry point of the program
- *
  * _islower - checks if the character is a lowercase letter
- *
- * @c: the character to be checkedddd 
- *
+ * @c: the character to be checkeddd
  * Return: 1 if @c is a lowercase letter, 0 otherwise
- *
  */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
