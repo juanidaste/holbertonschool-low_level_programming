e04b00b01544c0b9daa7f9aad338a745ce3aa07c
juanidaste <juanidaste@gmail.com>
2024-10-28 00:48:22 +0000

    commit 3

--- functions_nested_loops/5-main.c
+++ functions_nested_loops/5-main.c
@@ -1,27 +1,33 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * _isalpha - Entry point of the program
+ * main - check the code.
  *
- * _isalpha - checks if the character is a letter
- *
- * @c: the character to be checked
- *
- * Return: 1 if @c is a letter, lowercase or upper, 0 otherwise
- *
+ * Return: Always 0.
  */
-int _isalpha(int c)
+int main(void)
 {
-	if (c >= 97 && c <= 122)
-{
-	return (1);
-}
-	else if (c >= 65 && c <= 90)
-{
-	return (1);
-}
-	else
-{
-	return (0);
-}
+    int r;
+
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }           
