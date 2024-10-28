0b62a70b2adf03f5a8b891ac450d8bf8348be3e3
juanidaste <juanidaste@gmail.com>
2024-10-28 00:34:13 +0000

    commit 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Function that prints the alphabet in lowercase 10 times
+ * Return: Void
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+	else
+	return (0);
+}
