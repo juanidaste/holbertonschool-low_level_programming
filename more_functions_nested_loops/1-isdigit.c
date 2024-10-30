90dc812224cc3ad235edbd37e9a8198e76502cef
juanidaste <juanidaste@gmail.com>
2024-10-30 13:47:43 +0000

    commit 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * 
- *
+ * _isdigit - entry of the program
+ * @c - ofnwionfwiof
+ * Return: 1 aojfoaj
  */
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	if(c >= 48 && c <= 57)
+	if (c >= 48 && c <= 57)
 {
-		return(1);
+		return (1);
 }
 	else
+{
+	return (0);
 }
-
+}
