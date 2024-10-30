4887705d19e68a515699e90b0f668d5750d48509
juanidaste <juanidaste@gmail.com>
2024-10-30 14:01:43 +0000

    commit 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * mul - multiplica a por b
+ * @c: part to be checked
+ * Return :Resultado
+ */
+
+int mul(int a, int b)
+{
+	int resultado;
+	resultado = a * b;
+	return resultado;		
+}
