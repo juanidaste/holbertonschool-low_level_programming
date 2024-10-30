ca6913f3039377377d89edd5bae88f29d34fab8f
juanidaste <juanidaste@gmail.com>
2024-10-30 16:00:40 +0000

    commit 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry  point
+ * print_most_numbers - nfowhfown
+ */
+
+void print_most_numbers(void)
+{
+        int num;
+
+        for (num = 48; num <= 57; num++)
+{
+	if(num != 2 && num != 4)
+        _putchar(num);
+}
+        _putchar('\n');
+}
