a8fad80d996489e9a7e7f6e67b0a79ea37443549
juanidaste <juanidaste@gmail.com>
2024-10-30 16:12:04 +0000

    commit 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,14 @@
 
 void print_most_numbers(void)
 {
-        int num;
+	int num;
 
-        for (num = 48; num <= 57; num++)
-{
-	if(num != 2 && num != 4)
-{
-        _putchar(num);
-}
-}
-        _putchar('\n');
+	for (num = 48; num <= 57; num++)
+	{
+		if (num != '2' /* 2 en ascii -> 50 | 2 + '0' */ && num != '4')
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
 }
