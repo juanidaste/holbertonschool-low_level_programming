7e3973086ff0a2cef661dccbffea49132fcf85f3
juanidaste <juanidaste@gmail.com>
2024-10-31 14:18:24 +0000

    commit 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int stop;
+
+	while (stop != 11)
+	{
+		for (num = 0, <= 14, num++)
+		{
+
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			stop++;
+			_putchar('\n')
+		}
+	}
+}
