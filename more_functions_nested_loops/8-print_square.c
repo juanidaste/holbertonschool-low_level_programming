ea4ad35f83f64d4d5a139cc1f55280d09a01bbb8
juanidaste <juanidaste@gmail.com>
2024-11-01 14:06:42 +0000

    commit 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_square - Entry of the program
+ * @size: aidad
+ * print_square - ioneriwi
+ **/
+
+void print_square(int size)
+{
+	int i;
+	int w;
+
+	w = 0;
+
+	while (w < size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+			_putchar('\n');
+			w++;
+	}
+}
