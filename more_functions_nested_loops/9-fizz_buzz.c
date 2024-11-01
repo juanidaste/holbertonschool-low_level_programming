ed6b696fb899891eaad42dd6ae9293708d0f2f9e
juanidaste <juanidaste@gmail.com>
2024-11-01 15:37:19 +0000

    commit 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 #include "main.h"
 
-void fizzbuzz (int num)
+/**
+ * fizzbuzz - Entry of the program
+ * fizbuzz - lbdoid
+ * @num: idnwniw
+ **/
+
+void fizzbuzz(int num)
 {
 	int i;
 
-	for(i = num; i <= 100; i++)
+	for (i = num; i <= 100; i++)
 	{
 
-		if(i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 
-		else if(i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
 
-		else if(i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 
 		else
 		{
 			printf("%d", i);
 		}
-		if (i <100)
+		if (i < 100)
 		{
 			printf(" ");
 		}
 	}
 	printf("\n");
 }
+
+/**
+ * main - iubauiai
+ * Return: jdainad
+ **/
 
 int main(void)
 {
