ea5ad4591d554972dfa6b2fca85b483d46a64df2
juanidaste <juanidaste@gmail.com>
2024-11-04 02:37:39 +0000

    commit 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include <main.h>
+
+/**
+ * int_strlen - Entry of the program
+ * @len: idjiajdijad
+ * */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while(s[len != '\n'])
+	{
+		len++;
+	}
+	return (len);
+}
