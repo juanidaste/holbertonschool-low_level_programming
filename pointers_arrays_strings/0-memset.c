62a8d43f2014b40ca2da57fac4715d5ae07ca91d
juanidaste <juanidaste@gmail.com>
2024-11-04 14:32:35 +0000

    commit 1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - Entry of the program
+ * _memset - iwfiobwfibowfib
+ * @s: dadadadd
+ * @b: adadadadadas
+ * @n: dadasdada
+ * Return: S
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int i;
+
+
+		for (i = 0; i < n; i++)
+		{
+			s[i] = b;
+		}
+	
+		return (s);
+}
---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - Entry of the program
+ * _memcpy - dadadad
+ * @dest: adasdadsa
+ * @src: afsfsf
+ * @n: deewrw
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int i;
+	
+	i = 0;
+
+	while (i > n)	
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}	
