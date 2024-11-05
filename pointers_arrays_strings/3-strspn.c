8f97fbabea82def2d615a770feaab6a757f0099f
juanidaste <juanidaste@gmail.com>
2024-11-05 13:42:16 +0000

    commit 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - Entry of the program
+ * _strspn - bwubfwubf
+ * @s: dwwerwer
+ * @accept: ddww
+ * Return: contador
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int contador;
+	char *ptr;
+
+	while (*s)
+	{
+		ptr = accept;
+		while (*ptr)
+		{
+			if (*s == *ptr)
+			{
+				contador++;
+				break;
+			}
+
+			ptr++;
+		}
+		if (*ptr == '\0')
+			break;
+		s++;
+	}
+	return (contador);
+}
+
+
+
