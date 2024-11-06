e2989e35d53da3167abda8038300db8de05eceaf
juanidaste <juanidaste@gmail.com>
2024-11-06 15:02:13 +0000

    commit 6

---
+++ pointers_arrays_strings/7-print_chessoard.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - Entry of the program
+ * @a: dadadadad
+ * Return: adadasd
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
