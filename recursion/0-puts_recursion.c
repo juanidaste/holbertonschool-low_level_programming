2ed5ab8c6b6452186a5f9da372c5e802a4361fee
juanidaste <juanidaste@gmail.com>
2024-11-06 16:02:53 +0000

    commit 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Entry of the program
+ * _puts_recursion - wfwfwf
+ * @s: fwfwf
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
+
