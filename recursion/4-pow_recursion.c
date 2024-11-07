488421200dbf8d4f478eef8e48812d15326c383a
juanidaste <juanidaste@gmail.com>
2024-11-07 16:57:27 +0000

    commit 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,10 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry of the program
+ * _pow_recursion - wijinf
+ * @x: eifef
+ * @y: cinicnio
+ * Return: mdk
+ */
+
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
-	if(y == 0)
+	if (y == 0)
 	{
 	return (1);
 	}
