15285bde07738c69c3b325d2f7495c980a09465c
juanidaste <juanidaste@gmail.com>
2024-11-07 12:46:54 +0000

    commit 3

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strlen_recursion - Entry of the progra
+ *_strlen_recursion - nninwfw
+ *@s: adqqq
+ *Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+		return(0);
+		_strlen_recursion(s + 1) + 1);
+}
