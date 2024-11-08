162422b4bbe741131c152866ff30979de5c22af5
juanidaste <juanidaste@gmail.com>
2024-11-08 21:30:03 +0000

    commit 5

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - Entry of the program
+ * is_prime_number - buii3buu
+ * @n: wiufbiwf
+ * Return: widfbuiw
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	i = 2;
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (1);
+}
+
