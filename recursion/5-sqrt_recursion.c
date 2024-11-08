a0264ba018aaab1073b12295ac488aa0eb8d4f39
juanidaste <juanidaste@gmail.com>
2024-11-08 18:57:26 +0000

    commit 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry of the program
+ * _sqrt_recursion - iefeoife}
+ * @n: eioejgmioe
+ * Return: gbegiue
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n * n == n)
+	{
+		return (n);
+	}
+	if (n * n > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_helper(n, num) + 1);
+}
