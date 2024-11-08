82e035ac01ca69b02c3500e4025b699059587314
juanidaste <juanidaste@gmail.com>
2024-11-08 19:10:02 +0000

    commit 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,18 +5,31 @@
  * _sqrt_recursion - Entry of the program
  * _sqrt_recursion - iefeoife}
  * @n: eioejgmioe
- * Return: gbegiue
+ * Return: ijfwpom
  */
+
+int _sqrt_helper(int n, int guess);
 
 int _sqrt_recursion(int n)
 {
-	if (n * n == n)
-	{
-		return (n);
-	}
-	if (n * n > n)
-	{
-		return (-1);
-	}
-	return (_sqrt_helper(n, num) + 1);
+    if (n < 0)
+    {
+        return -1;
+    }
+
+    return _sqrt_helper(n, 1);
+}
+
+int _sqrt_helper(int n, int guess)
+{
+    if (guess * guess == n)
+    {
+        return guess;
+    }
+    if (guess * guess > n)
+    {
+        return -1;
+    }
+
+    return _sqrt_helper(n, guess + 1);
 }
