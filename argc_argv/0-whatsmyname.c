cd37b0bbe52e33bc1ac90e67068f202e870870ec
juanidaste <juanidaste@gmail.com>
2024-11-10 18:53:44 +0000

    commit 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the program
+ * main - dfdqq
+ * @argv: asdad
+ * @argc: dadad
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
