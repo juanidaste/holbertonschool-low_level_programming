f43573c0e3cd9fa99a0d0da744453b4b3583e076
juanidaste <juanidaste@gmail.com>
2024-11-10 19:10:46 +0000

    commit 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * main - adadad
+ * @argc: dad
+ * @argv: fsfsfs
+ * Return: dad
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
