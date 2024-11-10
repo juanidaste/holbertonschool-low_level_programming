a3739fe8c2c211a7a21d32d2e2296a142422810f
juanidaste <juanidaste@gmail.com>
2024-11-10 19:27:03 +0000

    commit 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * main - dadadad
+ * @argv: dada
+ * @argc: dadad
+ * Return: asdad
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
