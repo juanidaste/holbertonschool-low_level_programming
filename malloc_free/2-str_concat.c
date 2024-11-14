50c202ff45832b9b38c71736888d4261c5cc2ceb
juanidaste <juanidaste@gmail.com>
2024-11-14 13:59:27 +0000

    commit 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,9 +13,12 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int len1, len2, i, j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
