e0a016bd5d8a46c533ae4c1f40f28659d2a1a9ce
juanidaste <juanidaste@gmail.com>
2024-11-17 22:49:06 +0000

    commit 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - age, name, owner
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ * @name: name of the dog
+ * Description: adadadddadadaddadadadadadadad
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
