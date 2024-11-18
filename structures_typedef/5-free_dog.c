d1165843ff51814b9121b7a9c2ad34b365d6fbd1
juanidaste <juanidaste@gmail.com>
2024-11-18 02:07:56 +0000

    commit 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * free_dog - asdasdadad
+ * @d: pdadasd
+ * Return: adasd
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
